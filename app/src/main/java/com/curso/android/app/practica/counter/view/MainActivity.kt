28e70f0988003a591e5d4eeec539c20ab48e2f50
Horacio Karzon <horaciokar@gmail.com>
2023-07-24 23:38:03 -0500

    laboratorio

--- app/src/main/java/com/curso/android/app/practica/counter/view/MainActivity.kt
+++ app/src/main/java/com/curso/android/app/practica/counter/view/MainActivity.kt
@@ -1,31 +1,38 @@
-package com.curso.android.app.practica.counter.view
-
-import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.curso.android.app.practica.counter.databinding.ActivityMainBinding
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.viewModels
+import com.curso.android.app.practica.counter.R
+import com.curso.android.app.practica.counter.view.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var editTextNumber1: EditText
+    private lateinit var editTextNumber2: EditText
+    private lateinit var btnComparar: Button
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        mainViewModel.counter.observe(this) {
-            println("Recibimos un nuevo valor de counter. $it")
-            binding.counter.text = "${it.number}"
-            binding.timestamp.text = "Actualizado ${it.timestamp}"
-        }
+        editTextNumber1 = findViewById(R.id.editText1)
+        editTextNumber2 = findViewById(R.id.editText2)
+        btnComparar = findViewById(R.id.btnComparar)
 
-        binding.incrementButton.setOnClickListener {
-            mainViewModel.incrementCounter()
-        }
-        binding.decrementButton.setOnClickListener {
-            mainViewModel.decrementCounter()
+        btnComparar.setOnClickListener {
+            val text1 = editTextNumber1.text.toString()
+            val text2 = editTextNumber2.text.toString()
+
+            viewModel.setText1(text1)
+            viewModel.setText2(text2)
+
+            val resultado = if (viewModel.areTextsEqual()) {
+                "Los textos son iguales"
+            } else {
+                "Los textos son diferentes"
+            }
         }
     }
 }
--- app/src/main/java/com/curso/android/app/practica/counter/view/MainViewModel.kt
+++ app/src/main/java/com/curso/android/app/practica/counter/view/MainViewModel.kt
@@ -1,7 +1,5 @@
 package com.curso.android.app.practica.counter.view
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.curso.android.app.practica.counter.model.Counter
@@ -10,24 +8,17 @@ import java.util.Date
 
 class MainViewModel: ViewModel() {
 
-    // Solo queremos que se pueda leer el contador
-    val counter: LiveData<Counter> get() = _counter
-    // no nos interesa que se modifique por fuera del ViewModel
-    private var _counter = MutableLiveData<Counter>(Counter(0, Date()))
+    private var text1 = ""
+    private var text2 = ""
 
-    fun incrementCounter() {
-        val next = (_counter.value?.number ?: 0) + 1
-        updateCounter(next)
+    fun setText1(text: String) {
+        text1 = text
     }
 
-    fun decrementCounter() {
-        val next = (_counter.value?.number ?: 0) - 1
-        updateCounter(next)
+    fun setText2(text: String) {
+        text2 = text
     }
-
-    private fun updateCounter(next: Int) {
-        viewModelScope.launch {
-            _counter.value = Counter(next, Date())
-        }
+    fun areTextsEqual(): Boolean {
+        return text1 == text2
     }
 }
